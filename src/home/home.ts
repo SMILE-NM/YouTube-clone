4cd09f8ffeda95b4f17f296902882de661cdb103
SMILE-NM <nematovmz2003@gmail.com>
2024-11-23 17:52:42 +0500

    Start YouTube project

---
+++ src/home/home.ts
@@ -0,0 +1,14 @@
+export const categories = [
+  'All',
+  'JavaScript',
+  'TypeScript',
+  'Programming',
+  'Weight Lifting',
+  'Bowling',
+  'Web Development',
+  'React',
+  'Vue js',
+  'Angular',
+  'Node.js',
+  'Python',
+];
